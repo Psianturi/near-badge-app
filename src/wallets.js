cfdd97d6b3a545e4afd59cfb0e386e8970411286
Psianturi <posma2018@gmail.com>
2025-09-09 19:00:53 +0700

    Update config and use VITE

--- src/wallets.js
+++ src/wallets.js
@@ -10,7 +10,8 @@ import { ContractName, NetworkId } from "./config";
 
 
 
-const fastNearApiKey = process.env.NEXT_PUBLIC_FASTNEAR_API_KEY;
+// const fastNearApiKey = process.env.NEXT_PUBLIC_FASTNEAR_API_KEY;
+const fastNearApiKey = import.meta.env.VITE_FASTNEAR_API_KEY;
 
 let nodeUrl;
 if (NetworkId === 'mainnet') {
